24857f809670bf17cd76025aee535a2fcf7c784b
dimerf99 <93322314+dimerf99@users.noreply.github.com>
2025-09-03 15:12:48 +0300

    Refactor MultiphysicsDataset

--- neuralop/data/datasets/multiphysicis_dataset.py
+++ neuralop/data/datasets/multiphysicis_dataset.py
@@ -8,7 +8,7 @@ from typing import List, Union
 from scipy.interpolate import RegularGridInterpolator
 
 from .tensor_dataset import TensorDataset
-from ..transforms.data_processors import DefaultDataProcessor, MultiphysicsDataProcessor
+from ..transforms.data_processors import MultiphysicsDataProcessor
 from ..transforms.normalizers import MultiphysicsUnitGaussianNormalizer
 
 
@@ -28,56 +28,161 @@ def load_data(root_dir, dataset_name, process_type, resolution, file_format=".pt
         raise ValueError(f"Unknown file format: {file_format}")
 
 
-class GridPreprocessor:
-    """ GridPreprocessor resizes and subsample raw data
-    """
+# class GridPreprocessor:
+#     """ GridPreprocessor resizes and subsample raw data
+#     """
+#
+#     def resize_to_common_grid_batch(self, data, target_grid_shape):
+#         _, *dim_shapes = data.shape
+#         x, y = [np.linspace(0, 1, dim_i) for dim_i in dim_shapes]
+#         interpolator_new = RegularGridInterpolator(
+#             (x, y), data
+#         )
+#         grid_new = np.stack(
+#             list(np.meshgrid(*(np.linspace(0, 1, target_grid_shape) for _ in range(len(dim_shapes))), indexing='ij')),
+#             axis=-1
+#         )
+#         return interpolator_new(grid_new)
+#
+#     def get_mode(self, data):
+#         # `nearest`,
+#         # `linear` (3D-only),
+#         # `bilinear`,
+#         # `bicubic` (4D-only),
+#         # `trilinear` (5D-only),
+#
+#         if data.dtype is torch.bool:
+#             return 'nearest'
+#
+#         if data.shape != 5:
+#             return 'bilinear'
+#         else:
+#             return 'trilinear'
+#
+#     def resize_to_common_grid(self, data, target_resolution, interpolate_mode):
+#         original_shape = data.shape
+#         original_ndim = data.ndim
+#
+#         if torch.all(torch.tensor(original_shape[1:]) == torch.tensor(target_resolution)):
+#             return data
+#
+#         # 4D (B, C, H, W)
+#         if original_ndim == 2:
+#             # (B, X) -> (B, 1, 1, X)
+#             data = data.unsqueeze(1).unsqueeze(2)
+#         elif original_ndim == 3:
+#             # (B, X, Y) -> (B, 1, X, Y)
+#             data = data.unsqueeze(1)
+#
+#         if interpolate_mode is None:
+#             interpolate_mode = self.get_mode(data)
+#
+#         data_resized = F.interpolate(
+#             data.float(),
+#             size=target_resolution,
+#             mode=interpolate_mode,
+#         )
+#
+#         if original_ndim == 2:
+#             data_resized = data_resized.squeeze(1).squeeze(2)
+#         elif original_ndim == 3:
+#             data_resized = data_resized.squeeze(1)
+#
+#         return data_resized
+#
+#     def subsample(self, data, subsampling_rate, n_train, channel_dim):
+#         data_dims = data.ndim - 2
+#
+#         if not subsampling_rate:
+#             subsampling_rate = 1
+#         if not isinstance(subsampling_rate, list):
+#             subsampling_rate = [subsampling_rate] * data_dims
+#         assert len(subsampling_rate) == data_dims, \
+#             f"Error: length mismatch between input_subsampling_rate and dimensions of data.\
+#                         input_subsampling_rate must be one int shared across all dims, or an iterable of\
+#                             length {len(data_dims)}, got {subsampling_rate}"
+#
+#         train_indices = [slice(0, n_train, None)] + [slice(None, None, rate) for rate in subsampling_rate]
+#         train_indices.insert(channel_dim, slice(None))
+#         return data[train_indices]
 
-    def resize_to_common_grid_batch(self, data, target_grid_shape):
-        _, *dim_shapes = data.shape
-        x, y = [np.linspace(0, 1, dim_i) for dim_i in dim_shapes]
-        interpolator_new = RegularGridInterpolator(
-            (x, y), data
-        )
-        grid_new = np.stack(
-            list(np.meshgrid(*(np.linspace(0, 1, target_grid_shape) for _ in range(len(dim_shapes))), indexing='ij')),
-            axis=-1
-        )
-        return interpolator_new(grid_new)
 
-    def resize_to_common_grid(self, data, target_resolution):
-        _, *res = data.shape
-        if torch.all(torch.tensor(res) == target_resolution):
-            return data
+def resize_to_common_grid_batch(data, target_grid_shape):
+    _, *dim_shapes = data.shape
+    x, y = [np.linspace(0, 1, dim_i) for dim_i in dim_shapes]
+    interpolator_new = RegularGridInterpolator(
+        (x, y), data
+    )
+    grid_new = np.stack(
+        list(np.meshgrid(*(np.linspace(0, 1, target_grid_shape) for _ in range(len(dim_shapes))), indexing='ij')),
+        axis=-1
+    )
+    return interpolator_new(grid_new)
 
-        if data.dtype is torch.bool:
-            return F.interpolate(
-                data.unsqueeze(1).float(),
-                size=target_resolution,
-                mode='nearest',
-            ).squeeze(1)
-        else:
-            return F.interpolate(
-                data.unsqueeze(1),
-                size=target_resolution,
-                mode='bilinear',
-                align_corners=False
-            ).squeeze(1)
 
-    def subsample(self, data, subsampling_rate, n_train, channel_dim):
-        data_dims = data.ndim - 2
+def get_mode(data):
+    # `nearest`,
+    # `linear` (3D-only),
+    # `bilinear`,
+    # `bicubic` (4D-only),
+    # `trilinear` (5D-only),
 
-        if not subsampling_rate:
-            subsampling_rate = 1
-        if not isinstance(subsampling_rate, list):
-            subsampling_rate = [subsampling_rate] * data_dims
-        assert len(subsampling_rate) == data_dims, \
-            f"Error: length mismatch between input_subsampling_rate and dimensions of data.\
-                        input_subsampling_rate must be one int shared across all dims, or an iterable of\
-                            length {len(data_dims)}, got {subsampling_rate}"
+    if data.dtype is torch.bool:
+        return 'nearest'
 
-        train_indices = [slice(0, n_train, None)] + [slice(None, None, rate) for rate in subsampling_rate]
-        train_indices.insert(channel_dim, slice(None))
-        return data[train_indices]
+    if data.shape != 5:
+        return 'bilinear'
+    else:
+        return 'trilinear'
+
+
+def resize_to_common_grid(data, target_resolution, interpolate_mode):
+    original_shape = data.shape
+    original_ndim = data.ndim
+
+    if torch.all(torch.tensor(original_shape[1:]) == torch.tensor(target_resolution)):
+        return data
+
+    # 4D (B, C, H, W)
+    if original_ndim == 2:
+        # (B, X) -> (B, 1, 1, X)
+        data = data.unsqueeze(1).unsqueeze(2)
+    elif original_ndim == 3:
+        # (B, X, Y) -> (B, 1, X, Y)
+        data = data.unsqueeze(1)
+
+    if interpolate_mode is None:
+        interpolate_mode = get_mode(data)
+
+    data_resized = F.interpolate(
+        data.float(),
+        size=target_resolution,
+        mode=interpolate_mode,
+    )
+
+    if original_ndim == 2:
+        data_resized = data_resized.squeeze(1).squeeze(2)
+    elif original_ndim == 3:
+        data_resized = data_resized.squeeze(1)
+
+    return data_resized
+
+
+def subsample(data, subsampling_rate, n_train, channel_dim):
+    data_dims = data.ndim - 2
+
+    if not subsampling_rate:
+        subsampling_rate = 1
+    if not isinstance(subsampling_rate, list):
+        subsampling_rate = [subsampling_rate] * data_dims
+    assert len(subsampling_rate) == data_dims, \
+        f"Error: length mismatch between input_subsampling_rate and dimensions of data.\
+                    input_subsampling_rate must be one int shared across all dims, or an iterable of\
+                        length {len(data_dims)}, got {subsampling_rate}"
+
+    train_indices = [slice(0, n_train, None)] + [slice(None, None, rate) for rate in subsampling_rate]
+    train_indices.insert(channel_dim, slice(None))
+    return data[train_indices]
 
 
 class MultiphysicsDataset:
@@ -110,6 +215,7 @@ class MultiphysicsDataset:
                  encode_input: bool = False,
                  encode_output: bool = True,
                  encoding="channel-wise",
+                 interpolate_mode: str = None,
                  input_subsampling_rate=None,
                  output_subsampling_rate=None,
                  channel_dim=1,
@@ -164,13 +270,12 @@ class MultiphysicsDataset:
         self.batch_size = batch_size
         self.test_resolutions = test_resolutions
         self.test_batch_sizes = test_batch_sizes
-        self.grid_preprocessor = GridPreprocessor()
 
         # Load train data
         data = load_data(root_dir, dataset_name, 'train', train_resolution, file_format='.pt')
 
-        data["x"] = self.grid_preprocessor.resize_to_common_grid(data["x"], train_resolution)
-        data["y"] = self.grid_preprocessor.resize_to_common_grid(data["y"], train_resolution)
+        data["x"] = resize_to_common_grid(data["x"], train_resolution, interpolate_mode)
+        data["y"] = resize_to_common_grid(data["y"], train_resolution, interpolate_mode)
 
         x_train = data["x"].type(torch.float32).clone()
         y_train = data["y"].type(torch.float32).clone()
@@ -179,8 +284,8 @@ class MultiphysicsDataset:
             x_train = x_train.unsqueeze(channel_dim)
             y_train = y_train.unsqueeze(channel_dim)
 
-        x_train = self.grid_preprocessor.subsample(x_train, input_subsampling_rate, n_train, channel_dim)
-        y_train = self.grid_preprocessor.subsample(y_train, output_subsampling_rate, n_train, channel_dim)
+        x_train = subsample(x_train, input_subsampling_rate, n_train, channel_dim)
+        y_train = subsample(y_train, output_subsampling_rate, n_train, channel_dim)
 
         del data
 
@@ -232,8 +337,8 @@ class MultiphysicsDataset:
 
             data = load_data(root_dir, dataset_name, 'test', res, file_format='.pt')
 
-            data["x"] = self.grid_preprocessor.resize_to_common_grid(data["x"], res)
-            data["y"] = self.grid_preprocessor.resize_to_common_grid(data["y"], res)
+            data["x"] = resize_to_common_grid(data["x"], res, interpolate_mode)
+            data["y"] = resize_to_common_grid(data["y"], res, interpolate_mode)
 
             x_test = data["x"].type(torch.float32).clone()
             y_test = data["y"].type(torch.float32).clone()
@@ -242,8 +347,8 @@ class MultiphysicsDataset:
                 x_test = x_test.unsqueeze(channel_dim)
                 y_test = y_test.unsqueeze(channel_dim)
 
-            x_test = self.grid_preprocessor.subsample(x_test, input_subsampling_rate, n_test, channel_dim)
-            y_test = self.grid_preprocessor.subsample(y_test, output_subsampling_rate, n_test, channel_dim)
+            x_test = subsample(x_test, input_subsampling_rate, n_test, channel_dim)
+            y_test = subsample(y_test, output_subsampling_rate, n_test, channel_dim)
 
             del data
 
